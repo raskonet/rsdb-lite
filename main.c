47447273c47b364799d767eb267d05784dc13de3
raskonet <shettyrakshith610@gmail.com>
2025-03-24 16:05:52 +0530

    added tables

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
+
+#define size_of_attribute(Struct,Attribute) sizeof(((Struct*)0)->Attribute)
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+const u32int_t ID_SIZE=size_of_attribute(Row,id);
+const u32int_t USERNAME_SIZE=size_of_attribute(Row,username);
+const u32int_t EMAIL_SIZE=size_of_attribute(Row,email);
+const u32int_t ID_OFFSET=0;
+const u32int_t USERNAME_OFFSET=ID_SIZE+ID_SIZE;
+const u32int_t EMAIL_OFFSET = USERNAME_OFFSET+USERNAME_SIZE;
+const u32int_t ROW_SIZE = EMAIL_SIZE+ID_SIZE+USERNAME_SIZE;
 
 typedef struct{
   char* buffer;
@@ -25,8 +39,40 @@ typedef enum{
 }StatementType;
 
 typedef struct{
+  u32int_t id;
+  char* username[COLUMN_USERNAME_SIZE];
+  char* email[COLUMN_EMAIL_SIZE];
+}Row;
+
+typedef struct{
   StatementType type;
+  Row row_to_insert;
 }Statement;
+
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialise_row(void* source,Row* destination){
+    memcpy(&(destination->id),source+ID_OFFSET,ID_SIZE);
+    memcpy(&(destination->username),source+USERNAME_OFFSET,USERNAME_SIZE);
+    memcpy(&(destination->email),source+EMAIL_OFFSET,EMAIL_SIZE);
+}
+
+//TABLE STRUCTURE FROM HERE >>>
+#define TABLE_MAX_PAGES 100
+const u32int_t PAGE_SIZE=4096;
+const u32int_t ROWS_PER_PAGE=PAGE_SIZE/ROW_SIZE;
+const u32int_t ROWS_PER_TABLE=TABLE_MAX_PAGES*ROWS_PER_PAGE;
+
+typedef struct{
+  u32int_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+}Table;
+//TABLE DEFINNITION ENDS HERE
 
 void print_prompt(){
   printf("db > ");
@@ -129,9 +175,4 @@ int main(int argc,char* argv[]){
   printf("executed..\n");
   }
 
-  
-
 }
-
-
-
